da1f74f82b3f3c69477e03e9c2287c3a57aaa7ca
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-05-13 16:33:18 -0400

    All files and UI refactored

--- js/app.js
+++ js/app.js
@@ -1,109 +1,115 @@
-// Variables to hold user numbers
-let firstNum;
-let secondNum;
-// Operator variable
-let addOperator = "add";
-let subtractOperator = "subtract";
-let multiplyOperator = "multiply";
-let divideOperator = "divide";
-let operator = "divide";
-let periodDisplay = ".";
-
-// Select Operator buttons
-const operatorBtn = document.querySelectorAll(".operators");
-// Select Display
-const displayResult = document.querySelector(".display");
-// Select clear button
+// Number Buttons
+const numberBtn = document.querySelectorAll(".numbers");
+// Main display
+const displayCalculator = document.querySelector(".display");
+const equalsBtn = document.querySelector(".equalsBtn");
+// Clear
 const clearBtn = document.querySelector(".clearBtn");
-// Select numbers
-const numberBtn = document.querySelectorAll(".numberBtn");
+// Array
+const operatorArray = ["+", "÷", "-", "x"];
+
+// Number variables
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let displayNum;
+let sum = "";
+
+// Numbers ************
+numberBtn.forEach(numbers => {
+  numbers.addEventListener("click", e => {
+    displayNum = e.target.innerText;
+    const symbolInArray = operatorArray.includes(displayNum);
+    displayCalculator.innerText += displayNum;
+
+    if (firstNum && symbolInArray) {
+      operator = displayNum;
+    } else if (!operator) {
+      firstNum += displayNum;
+      console.log(` The first number is: ${firstNum}`);
+    } else if (operator) {
+      secondNum += displayNum;
+      if (secondNum) {
+        operate();
+      }
+      console.log(`The second number is: ${secondNum}`);
+    }
+  });
+});
+
+// Equals
+equalsBtn.addEventListener("click", operator => {
+  operate();
+  // firstNum = displayCalculator.innerText;
+  firstNum = displayCalculator.innerText;
+  secondNum = "";
+  symbolInArray = "";
+});
 
 // Clear display
 clearBtn.addEventListener("click", () => {
-  displayResult.textContent = ``;
+  displayCalculator.innerText = "";
+  // firstNum.innerText = "";
+  // secondNum.innerText = "";
+  firstNum = "";
+  secondNum = "";
 });
 
-// Main display /////////
-numberBtn.forEach(numbers => {
-  numbers.addEventListener("click", e => {
-    let equals = false;
-    num1 = e.target.dataset.id;
-    displayResult.textContent += `${num1}`;
-  });
-});
-
-/////////  MAIN OPERATOR FUNCTION   /////////////
-operatorBtn.forEach(btnOp => {
-  btnOp.addEventListener("click", e => {
-    let operator = e.target.dataset.id;
-    console.log(operator);
-
-    // switch(operator) {
-    //  case "add":
-
-    // }
-  });
-});
-// Operate function calls related function to
-// perform calculations and display results
-// Calls the approprite operator function
-function operate(operator, firstNum, secondNum) {
+// Operate Function
+const operate = () => {
   switch (operator) {
-    case "add":
+    case "+":
       add(firstNum, secondNum);
       break;
-    case "subtract":
+    case "-":
       subtract(firstNum, secondNum);
       break;
-    case "multiply":
+    case "x":
       multiply(firstNum, secondNum);
       break;
-    case "divide":
+    case "÷":
       divide(firstNum, secondNum);
       break;
     default:
-      console.log("Got nothing!");
+      console.log("Nothing to calculate!");
   }
-}
-
-///////// CALCULATOR FUNCTIONS //////////////////
-
-// Operator functions
-const add = (num1, num2) => {
-  let sum = num1 + num2;
-  console.log(`Add result is: ${sum}`);
-  return sum;
+  firstNum = sum;
+  secondNum = "";
+  symbolInArray = "";
 };
-const subtract = (num1, num2) => {
-  let sum = num1 - num2;
-  console.log(`Subtract result is: ${sum}`);
-  return sum;
-};
-const multiply = (num1, num2) => {
-  let sum = num1 * num2;
-  console.log(`Multply result is: ${sum}`);
-  return sum;
-};
-const divide = (num1, num2) => {
-  let sum = num1 / num2;
-  console.log(`Divide result is: ${sum}`);
-  return sum;
-};
-// multiply function
-// function multiply(num1, num2) {
-//   sum = num1 * num2;
-//   console.log(`Result is: ${sum}`);
-// }
-// divide function
-// function divide(num1, num2) {
-//   sum = num1 / num2;
-//   console.log(`Result is: ${sum}`);
-// }
 
-// Display Function to show results
-function displayCalculation() {}
+// Calculation Functions
+const add = (firstNum, secondNum) => {
+  firstNum = parseInt(firstNum);
+  secondNum = parseInt(secondNum);
+  sum = firstNum + secondNum;
+  console.log(sum);
+  firstNum = "";
+  secondNum = "";
+  console.log(`The first num is now: ${sum}`);
+  return (displayCalculator.innerText = sum);
+};
+const subtract = (firstNum, secondNum) => {
+  firstNum = parseFloat(firstNum);
+  secondNum = parseFloat(secondNum);
+  sum = firstNum - secondNum;
+  console.log(sum);
+  return (displayCalculator.innerText = sum);
+};
+const multiply = (firstNum, secondNum) => {
+  firstNum = parseFloat(firstNum);
+  secondNum = parseFloat(secondNum);
+  sum = firstNum * secondNum;
+  console.log(sum);
+  return (displayCalculator.innerText = sum);
+};
+const divide = (firstNum, secondNum) => {
+  firstNum = parseFloat(firstNum);
+  secondNum = parseFloat(secondNum);
+  sum = firstNum / secondNum;
+  console.log(sum);
+  return (displayCalculator.innerText = sum);
+};
 
-operate(divideOperator, 5, 2);
-operate(addOperator, 5, 2);
-operate(subtractOperator, 5, 2);
-operate(multiplyOperator, 5, 2);
+// Display Function
+const displayResult = () => {};
